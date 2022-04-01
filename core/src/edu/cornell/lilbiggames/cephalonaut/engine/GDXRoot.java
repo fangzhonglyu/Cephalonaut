39b01c788c15ded2b8d9fe11d2b265fdd8933db9
mce58 <mce58@cornell.edu>
2022-03-31 20:37:10 -0400

    I don't think we need the inner kill all sound

--- core/src/edu/cornell/lilbiggames/cephalonaut/engine/GDXRoot.java
+++ core/src/edu/cornell/lilbiggames/cephalonaut/engine/GDXRoot.java
@@ -148,7 +148,6 @@ public class GDXRoot extends Game implements ScreenListener {
 		} else if (exitCode == LevelController.COMPLETE_LEVEL) {
 			playMode.setComplete(true);
 			playMode.setFailure(false);
-			SoundController.killAllSound();
 			setScreen(levelCompleteMode);
 		}
 	}
