c77a0dac2a570111fbd0908ca2bc8fb2271c597b
Ojmatte <ojm23@cofnell.edu>
2022-05-21 00:08:16 -0400

    Added star time for every level

--- desktop/src/edu/cornell/lilbiggames/cephalonaut/desktop/DesktopLauncher.java
+++ desktop/src/edu/cornell/lilbiggames/cephalonaut/desktop/DesktopLauncher.java
@@ -42,7 +42,7 @@ public class DesktopLauncher {
 		config.addIcon("A-michael-still.png", Files.FileType.Internal);
 //		config.vSyncEnabled = false;
 		config.foregroundFPS = 60;
-		config.useHDPI = true;
+		config.useHDPI = false;
 		new LwjglApplication(new GDXRoot(), config);
 	}
 }
